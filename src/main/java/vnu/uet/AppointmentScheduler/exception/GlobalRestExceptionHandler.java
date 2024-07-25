922f4361e6516997992ab09b245718a488453dd0
Le Viet Dat <lvdat.roy@gmail.com>
2024-07-25 08:48:34 +0700

    add global exception handler

---
+++ src/main/java/vnu/uet/AppointmentScheduler/exception/GlobalRestExceptionHandler.java
@@ -0,0 +1,27 @@
+package vnu.uet.AppointmentScheduler.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@Slf4j
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@RestControllerAdvice
+public class GlobalRestExceptionHandler {
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatusException(ResponseStatusException exc) {
+        log.error(exc.toString());
+        return new ResponseEntity<>(exc.getMessage(), exc.getStatusCode());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleUnexpectedException(Exception exc) {
+        log.error(exc.toString());
+        return new ResponseEntity<>("An unexpected error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
