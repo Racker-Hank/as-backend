cff6b09a20d4f28031d001b7b8d950ca79fa885b
Le Viet Dat <lvdat.roy@gmail.com>
2024-08-15 14:07:10 +0700

    add /auth/me endpoint #18, replace constants in /login by environment values

--- src/main/java/vnu/uet/AppointmentScheduler/controller/user/AuthController.java
+++ src/main/java/vnu/uet/AppointmentScheduler/controller/user/AuthController.java
@@ -5,16 +5,23 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import vnu.uet.AppointmentScheduler.constants.UserRole;
 import vnu.uet.AppointmentScheduler.dto.auth.LoginRequestDTO;
 import vnu.uet.AppointmentScheduler.dto.auth.RegisterDoctorRequestDTO;
 import vnu.uet.AppointmentScheduler.dto.auth.RegisterPatientRequestDTO;
 import vnu.uet.AppointmentScheduler.middleware.auth.AuthService;
+import vnu.uet.AppointmentScheduler.model.user.User;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -26,6 +33,8 @@ public class AuthController {
 
 	@PostMapping(value = "/login")
 	public ResponseEntity<String> login(
+		@Value("${CONTEXT_PATH}") String contextPath,
+		@Value("${JWT_EXPIRATION}") Long jwtExpiration,
 		@RequestBody LoginRequestDTO loginRequestDTO,
 		HttpServletResponse response
 	) {
@@ -37,8 +46,8 @@ public class AuthController {
 		ResponseCookie cookie = ResponseCookie.from(ACCESS_TOKEN_COOKIE_KEY, jwtToken)
 			.httpOnly(true)
 			.secure(false)
-			.path("/api")
-			.maxAge(7 * 24 * 60 * 60)
+			.path(contextPath)
+			.maxAge(jwtExpiration)
 			.build();
 
 		response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
@@ -64,6 +73,25 @@ public class AuthController {
 		}
 
 		return new ResponseEntity<>("Auth token not found", HttpStatus.UNAUTHORIZED);
+	}
+
+	@GetMapping("/me")
+	public ResponseEntity<?> authenticateMe() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		if (authentication == null || !authentication.isAuthenticated()) {
+			return new ResponseEntity<>("User is not authenticated", HttpStatus.UNAUTHORIZED);
+		}
+
+		User userDetails = (User) authentication.getPrincipal();
+		log.debug(userDetails.toString());
+
+		Map<String, Object> responseBody = new HashMap<>();
+		responseBody.put("id", userDetails.getId());
+		responseBody.put("email", userDetails.getEmail());
+		responseBody.put("userRole", userDetails.getUserRole());
+
+		return ResponseEntity.ok(responseBody);
 	}
 
 	@PostMapping(value = "/register/doctor")
